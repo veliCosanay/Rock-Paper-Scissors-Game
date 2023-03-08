ca3e497bae31dd52a5c52b94b917a3780bc25639
veliCosanay <velicosanay06@gmail.com>
2023-03-08 23:12:23 +0300

    .

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -10,51 +10,51 @@ options = ["Rock","Paper","Scissors"]
 def computer_choise():
     return random.choice(options)
 
-def user_input_corrector(user_input):
-    if user_input == "R":
+def player_input_corrector(player_input):
+    if player_input == "R":
         return "Rock"
-    elif user_input == "P":
+    elif player_input == "P":
         return "Paper"
-    elif user_input == "S":
+    elif player_input == "S":
         return "Scissors"
 
-def results(computer_choise,user_input):
-    if computer_win_status[computer_choise] == user_input_corrector(user_input):
+def results(computer_choise,player_input):
+    if computer_win_status[computer_choise] == player_input_corrector(player_input):
         return "c"
-    elif computer_choise == user_input_corrector(user_input):
+    elif computer_choise == player_input_corrector(player_input):
         return "d"
     else:
-        return "u"
+        return "p"
 
 def main():
     computer_score = 0
-    user_score = 0
+    player_score = 0
 
-    # print("Taş için 'T', kağıt için 'K', Scissors için 'M' ye basınız")
+    
     print("Press 'R' for stone, 'P' for paper and 'S' for scissors")
 
     while True:
-        user_input = input("")
-        user_input = user_input.capitalize()
-        if not (user_input == "R" or user_input == "P" or user_input == "S"):
+        player_input = input("")
+        player_input = player_input.capitalize()
+        if not (player_input == "R" or player_input == "P" or player_input == "S"):
             print("Please select one of the options")
             continue
         computer_move = computer_choise()
-        if results(computer_move,user_input) == "c":
+        if results(computer_move,player_input) == "c":
             computer_score += 1
-        elif results(computer_move,user_input) == "u":
-            user_score += 1 
+        elif results(computer_move,player_input) == "p":
+            player_score += 1 
         
         print()
         print(f"Choise of computer: {computer_move}")
-        print(f"Choise of player: {user_input_corrector(user_input)}")
-        print(f"Computer {computer_score} - {user_score} Player")            
+        print(f"Choise of player: {player_input_corrector(player_input)}")
+        print(f"Computer {computer_score} - {player_score} Player")            
         print()
 
         if computer_score == 3:
             print("Computer Won")
             break
-        elif user_score == 3:
+        elif player_score == 3:
             print("Player won")
             break
 
